eee9fa557403804ff93c45a875836c2a6f2b7ec1
kevinjmosley93 <kevinjmosley93@gmail.com>
2021-06-19 19:13:36 -0500

    commit

--- components/Team.js
+++ components/Team.js
@@ -27,22 +27,22 @@ export const Team = () => {
   return (
     <Container>
       <h2 className='pb-2  fw-bold lh-1 border-bottom mb-5'>Meet Our Team</h2>
-      <section class='testimonials text-center'>
-        <div class='row align-items-center'>
+      <section className='testimonials text-center'>
+        <div className='row align-items-center'>
           {teamMembers.map(({ name, title, bio, img }, idx) => (
-            <div key={idx} class='col-lg-4'>
-              <div class='testimonial-item mx-auto mb-5 mb-lg-0'>
+            <div key={idx} className='col-lg-4'>
+              <div className='testimonial-item mx-auto mb-5 mb-lg-0'>
                 <img
-                  style={{ objectFit: 'contain' }}
-                  width={100}
-                  height={100}
-                  class='img-fluid rounded-circle mb-3'
+                  style={{ objectFit: 'cover' }}
+                  width={150}
+                  height={150}
+                  className='rounded-circle mb-3'
                   src={img}
                   alt='H.O.O.D Team Member'
                 />
                 <h3>{name}</h3>
                 <small className='fw-bold'>{title}</small>
-                <p class='font-weight-light mb-0'>{bio}</p>
+                <p className='font-weight-light mb-0'>{bio}</p>
               </div>
             </div>
           ))}
